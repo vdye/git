02652b2d07a6b078eeae9dd2d9685bf5b5af4eca
Jeff Hostetler <jeffhost@microsoft.com>
2020-04-10 21:14:44 -0400

    status: disable deserialize when verbose output requested.
    Disable deserialization when verbose output requested.
    
    Verbose mode causes Git to print diffs for modified files.
    This requires the index to be loaded to have the currently
    staged OID values.  Without loading the index, verbose output
    make it look like everything was deleted.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -43,6 +43,7 @@
 #include "commit-reach.h"
 #include "commit-graph.h"
 #include "pretty.h"
+#include "trace2.h"
 
 static const char * const builtin_commit_usage[] = {
 	N_("git commit [-a | --interactive | --patch] [-s] [-v] [-u<mode>] [--amend]\n"
@@ -1716,6 +1717,22 @@ int cmd_status(int argc, const char **argv, const char *prefix)
 	 */
 	try_deserialize = (!do_serialize &&
 			   (do_implicit_deserialize || do_explicit_deserialize));
+
+	/*
+	 * Disable deserialize when verbose is set because it causes us to
+	 * print diffs for each modified file, but that requires us to have
+	 * the index loaded and we don't want to do that (at least not now for
+	 * this seldom used feature).  My fear is that would further tangle
+	 * the merge conflict with upstream.
+	 *
+	 * TODO Reconsider this in the future.
+	 */
+	if (try_deserialize && verbose) {
+		trace2_data_string("status", the_repository, "deserialize/reject",
+				   "args/verbose");
+		try_deserialize = 0;
+	}
+
 	if (try_deserialize)
 		goto skip_init;
 
