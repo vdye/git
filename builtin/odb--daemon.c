c20b5b80380d1ac3d3e7d06f3759cac64ea88753
Jeff Hostetler <jeffhost@microsoft.com>
2021-04-09 18:14:40 -0400

    odb--daemon: stub in --run framework
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/odb--daemon.c
+++ builtin/odb--daemon.c
@@ -3,6 +3,7 @@
 #include "parse-options.h"
 #include "simple-ipc.h"
 #include "thread-utils.h"
+#include "odb-over-ipc.h"
 
 enum my_mode {
 	MODE_UNDEFINED = 0,
@@ -42,9 +43,99 @@ int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 }
 #else
 
+/*
+ * Technically, we don't need to probe for an existing daemon process
+ * running on our named pipe / socket, since we could just try to
+ * create it and let it fail if the pipe/socket is busy.  However,
+ * probing first allows us to give a better error message (especially
+ * in the case where we disassociate from the terminal and fork into
+ * the background).
+ */
+static int is_daemon_listening(void)
+{
+	return odb_over_ipc__get_state() == IPC_STATE__LISTENING;
+}
+
+struct my_odb_ipc_state
+{
+};
+
+static struct my_odb_ipc_state *my_state;
+
+static ipc_server_application_cb odb_cb;
+
+static int odb_cb(void *data, const char *command,
+		  ipc_server_reply_cb *reply_cb,
+		  struct ipc_server_reply_data *reply_data)
+{
+	struct my_odb_ipc_state *state = data;
+
+	assert(state == my_state);
+
+	// TODO respond to request from client.
+
+	return 0;
+}
+
+/*
+ * Use the simple SYNC interface, start the IPC thread pool, and
+ * block the calling thread until it shuts down.
+ */
+static int do_ipc(void)
+{
+	int ret;
+	const char *path = odb_over_ipc__get_path();
+
+	struct ipc_server_opts ipc_opts = {
+		.nr_threads = my_args.nr_ipc_threads,
+	};
+
+	ret = ipc_server_run(path, &ipc_opts, odb_cb, my_state);
+
+	if (ret == -2) /* maybe we lost a startup race */
+		error(_("IPC socket/pipe already in use: '%s'"), path);
+
+	else if (ret == -1)
+		error(_("could not start IPC thread pool on: '%s'"), path);
+
+	return ret;
+}
+
+/*
+ * Actually run the daemon.
+ */
+static int do_run_daemon(void)
+{
+	int ret;
+
+	// TODO Create mutexes and locks
+	//
+	// TODO Load up the packfiles
+	//
+	// TODO Consider starting a thread to watch for new/deleted packfiles
+	// TODO and update the in-memory database.
+
+	ret = do_ipc();
+
+	// TODO As a precaution, send stop events to our other threads and
+	// TODO JOIN them.
+	//
+	// TODO destroy mutexes and locks.
+	//
+	// TODO destroy in-memory databases.
+
+	return ret;
+}
+
+/*
+ * Try to run the odb--daemon in the foreground of the current process.
+ */
 static int try_run_daemon(void)
 {
-	return 0;
+	if (is_daemon_listening())
+		die("odb--daemon is already running");
+
+	return do_run_daemon();
 }
 
 int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
