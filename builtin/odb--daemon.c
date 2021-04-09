e7a046d84c6447ba3b66e4cfec386ecbcfb9967a
Jeff Hostetler <jeffhost@microsoft.com>
2021-04-09 16:57:41 -0400

    odb--daemon: stub in --run --ipc-threads
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/odb--daemon.c
+++ builtin/odb--daemon.c
@@ -2,13 +2,33 @@
 #include "config.h"
 #include "parse-options.h"
 #include "simple-ipc.h"
+#include "thread-utils.h"
+
+enum my_mode {
+	MODE_UNDEFINED = 0,
+	MODE_RUN,
+};
 
 static const char * const my_usage[] = {
+	"git odb--daemon --run [<options>]",
 	"git odb--daemon TBD...",
 	NULL
 };
 
+struct my_args
+{
+	enum my_mode mode;
+	int nr_ipc_threads;
+};
+
+static struct my_args my_args;
+
 static struct option my_options[] = {
+	OPT_CMDMODE('r', "run", &my_args.mode,
+		    N_("run the ODB daemon in the foreground"), MODE_RUN),
+
+	OPT_GROUP(N_("Daemon options")),
+	OPT_INTEGER(0, "ipc-threads", &my_args.nr_ipc_threads, N_("use <n> ipc threads")),
 	OPT_END()
 };
 
@@ -22,6 +42,10 @@ int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 }
 #else
 
+static int try_run_daemon(void)
+{
+	return 0;
+}
 
 int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 {
@@ -32,6 +56,15 @@ int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 
 	argc = parse_options(argc, argv, prefix, my_options, my_usage, 0);
 
-	die(_("Unhandled command mode"));
+	if (my_args.nr_ipc_threads < 1)
+		my_args.nr_ipc_threads = online_cpus();
+
+	switch (my_args.mode) {
+	case MODE_RUN:
+		return !!try_run_daemon();
+
+	default:
+		die(_("Unhandled command mode"));
+	}
 }
 #endif /* SUPPORTS_SIMPLE_IPC */
