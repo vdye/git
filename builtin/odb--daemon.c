d59f78fbd4ea464729dd214ceb1eab2a57b78e76
Jeff Hostetler <jeffhost@microsoft.com>
2021-04-12 09:38:50 -0400

    odb-over-ipc: add --stop command
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- builtin/odb--daemon.c
+++ builtin/odb--daemon.c
@@ -7,11 +7,13 @@
 
 enum my_mode {
 	MODE_UNDEFINED = 0,
-	MODE_RUN,
+	MODE_RUN, /* run daemon in current process */
+	MODE_STOP, /* stop an existing daemon */
 };
 
 static const char * const my_usage[] = {
-	"git odb--daemon --run [<options>]",
+	"git odb--daemon --run  [<options>]",
+	"git odb--daemon --stop [<options>]",
 	"git odb--daemon TBD...",
 	NULL
 };
@@ -25,8 +27,10 @@ struct my_args
 static struct my_args my_args;
 
 static struct option my_options[] = {
-	OPT_CMDMODE('r', "run", &my_args.mode,
+	OPT_CMDMODE(0, "run", &my_args.mode,
 		    N_("run the ODB daemon in the foreground"), MODE_RUN),
+	OPT_CMDMODE(0, "stop", &my_args.mode,
+		    N_("stop an existing ODB daemon"), MODE_STOP),
 
 	OPT_GROUP(N_("Daemon options")),
 	OPT_INTEGER(0, "ipc-threads", &my_args.nr_ipc_threads, N_("use <n> ipc threads")),
@@ -63,15 +67,36 @@ struct my_odb_ipc_state
 
 static struct my_odb_ipc_state my_state;
 
-static ipc_server_application_cb odb_cb;
+static ipc_server_application_cb odb_ipc_cb;
 
-static int odb_cb(void *data, const char *command,
+static int odb_ipc_cb(void *data, const char *command,
 		  ipc_server_reply_cb *reply_cb,
 		  struct ipc_server_reply_data *reply_data)
 {
 	struct my_odb_ipc_state *state = data;
 
 	assert(state == &my_state);
+
+	/*
+	 * We expect `command` to be of the form:
+	 *
+	 * <command> := quit NUL
+	 *            | TBC...
+	 */
+
+	if (!strcmp(command, "quit")) {
+		/*
+		 * A client has requested (over the pipe/socket) that this
+		 * daemon shutdown.
+		 *
+		 * Tell the IPC thread pool to shutdown (which completes the
+		 * `await` in the main thread and causes us to shutdown).
+		 *
+		 * In this callback we are NOT in control of the life of this
+		 * thread, so we cannot directly shutdown here.
+		 */
+		return SIMPLE_IPC_QUIT;
+	}
 
 	// TODO respond to request from client.
 
@@ -88,7 +113,7 @@ static int launch_ipc_thread_pool(void)
 	};
 
 	ret = ipc_server_run_async(&my_state.ipc_state,
-				   path, &ipc_opts, odb_cb, &my_state);
+				   path, &ipc_opts, odb_ipc_cb, &my_state);
 
 	if (ret == -2) /* maybe we lost a startup race */
 		error(_("IPC socket/pipe already in use: '%s'"), path);
@@ -138,6 +163,35 @@ static int try_run_daemon(void)
 	return do_run_daemon();
 }
 
+/*
+ * Acting as a CLIENT.
+ *
+ * Send a "quit" command to an existing `git odb--daemon` process
+ * (if running) and wait for it to shutdown.
+ *
+ * The wait here is important for helping the test suite be stable.
+ */
+static int client_send_stop(void)
+{
+	struct strbuf answer = STRBUF_INIT;
+	int ret;
+
+	ret = odb_over_ipc__command("quit", &answer);
+
+	/* The quit command does not return any response data. */
+	strbuf_release(&answer);
+
+	if (ret)
+		return ret;
+
+	trace2_region_enter("odb_client", "polling-for-daemon-exit", NULL);
+	while (odb_over_ipc__get_state() == IPC_STATE__LISTENING)
+		sleep_millisec(50);
+	trace2_region_leave("odb_client", "polling-for-daemon-exit", NULL);
+
+	return 0;
+}
+
 int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 {
 	if (argc == 2 && !strcmp(argv[1], "-h"))
@@ -153,6 +207,9 @@ int cmd_odb__daemon(int argc, const char **argv, const char *prefix)
 	switch (my_args.mode) {
 	case MODE_RUN:
 		return !!try_run_daemon();
+
+	case MODE_STOP:
+		return !!client_send_stop();
 
 	default:
 		die(_("Unhandled command mode"));
--- odb-over-ipc.c
+++ odb-over-ipc.c
@@ -28,8 +28,34 @@ enum ipc_active_state odb_over_ipc__get_state(void)
 
 int odb_over_ipc__command(const char *command, struct strbuf *answer)
 {
-	// TODO
-	return -1;
+	struct ipc_client_connection *connection = NULL;
+	struct ipc_client_connect_options options
+		= IPC_CLIENT_CONNECT_OPTIONS_INIT;
+	int ret;
+	enum ipc_active_state state;
+
+	strbuf_reset(answer);
+
+	options.wait_if_busy = 1;
+	options.wait_if_not_found = 0;
+
+	state = ipc_client_try_connect(odb_over_ipc__get_path(), &options,
+				       &connection);
+	if (state != IPC_STATE__LISTENING) {
+		die("odb--daemon is not running");
+		return -1;
+	}
+
+	ret = ipc_client_send_command_to_connection(connection, command, answer);
+	ipc_client_close_connection(connection);
+
+	if (ret == -1) {
+		die("could not send '%s' command to odb--daemon",
+		    command);
+		return -1;
+	}
+
+	return 0;
 }
 
 #endif /* SUPPORTS_SIMPLE_IPC */
