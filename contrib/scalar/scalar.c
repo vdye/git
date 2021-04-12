19028e998d423798562e52700b40039d6ac39cf0
Derrick Stolee <dstolee@microsoft.com>
2021-04-12 14:15:36 -0400

    scalar register: set recommended config settings
    Let's start implementing the `register` command. With this commit,
    recommended settings are configured upon `scalar register`.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/scalar/scalar.c
+++ contrib/scalar/scalar.c
@@ -5,12 +5,89 @@
 #include "cache.h"
 #include "gettext.h"
 #include "parse-options.h"
+#include "config.h"
+
+static int set_recommended_config(const char *file)
+{
+	struct {
+		const char *key;
+		const char *value;
+	} config[] = {
+		{ "am.keepCR", "true" },
+		{ "commitGraph.generationVersion", "1" },
+		{ "core.autoCRLF", "false" },
+		{ "core.FSCache", "true" },
+		{ "core.logAllRefUpdates", "true" },
+		{ "core.multiPackIndex", "true" },
+		{ "core.preloadIndex", "true" },
+		{ "core.safeCRLF", "false" },
+		{ "credential.validate", "false" },
+		{ "feature.manyFiles", "false" },
+		{ "feature.experimental", "false" },
+		{ "fetch.unpackLimit", "1" },
+		{ "fetch.writeCommitGraph", "false" },
+		{ "gc.auto", "0" },
+		{ "gui.GCWarning", "false" },
+		{ "index.threads", "true" },
+		{ "index.version", "4" },
+		{ "maintenance.auto", "false" },
+		{ "merge.stat", "false" },
+		{ "merge.renames", "false" },
+		{ "pack.useBitmaps", "false" },
+		{ "pack.useSparse", "true" },
+		{ "receive.autoGC", "false" },
+		{ "reset.quiet", "true" },
+		{ "status.aheadBehind", "false" },
+#ifdef WIN32
+		/*
+		 * Windows-specific settings.
+		 */
+		{ "core.untrackedCache", "true" },
+		{ "core.filemode", "true" },
+#endif
+		{ NULL, NULL },
+	};
+	int i;
+
+	for (i = 0; config[i].key; i++) {
+		char *value;
+
+		if (file || git_config_get_string(config[i].key, &value)) {
+			trace2_data_string("scalar", the_repository, config[i].key, "created");
+			git_config_set_in_file_gently(file, config[i].key,
+						      config[i].value);
+		} else {
+			trace2_data_string("scalar", the_repository, config[i].key, "exists");
+			free(value);
+		}
+	}
+
+	return 0;
+}
+
+static int register_dir(const char *dir)
+{
+	char *config_path = dir ? xstrfmt("%s/.git/config", dir) : NULL;
+	int res = set_recommended_config(config_path);
+
+	free(config_path);
+	return res;
+}
+
+static int cmd_register(int argc, const char **argv)
+{
+	if (argc != 1 && argc != 2)
+		usage(_("scalar register [<worktree>]"));
+
+	return register_dir(argc < 2 ? NULL : argv[1]);
+}
 
 struct {
 	const char *name;
 	int (*fn)(int, const char **);
 	int needs_git_repo;
 } builtins[] = {
+	{ "register", cmd_register, 1 },
 	{ NULL, NULL},
 };
 
