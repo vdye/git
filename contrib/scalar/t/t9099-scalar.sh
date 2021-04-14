290d267585406bedb7d18447507add293094c6f3
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-04-15 00:50:59 +0200

    scalar: test `scalar clone`
    This commit adds a simple regression test, modeled after Git's own
    test suite.
    
    A more comprehensive functional (or: integration) test suite can be
    found at https://github.com/microsoft/scalar; There is no intention to
    port that fuller test suite to `contrib/scalar/`; Instead, it will still
    be used to verify the `scalar` functionality in Microsoft's Git fork.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/scalar/t/t9099-scalar.sh
+++ contrib/scalar/t/t9099-scalar.sh
@@ -14,4 +14,29 @@ test_expect_success 'scalar shows a usage' '
 	test_expect_code 129 scalar -h
 '
 
+test_expect_success 'set up repository to clone' '
+	test_commit first &&
+	test_commit second &&
+	test_commit third &&
+	git switch -c parallel first &&
+	mkdir -p 1/2 &&
+	test_commit 1/2/3 &&
+	git config uploadPack.allowFilter true &&
+	git config uploadPack.allowAnySHA1InWant true
+'
+
+test_expect_success 'scalar clone' '
+	second=$(git rev-parse --verify second:second.t) &&
+	scalar clone "file://$(pwd)" cloned &&
+	(
+		cd cloned/src &&
+		test_path_is_missing 1/2 &&
+		test_must_fail git rev-list --missing=print $second &&
+		git rev-list $second &&
+		git cat-file blob $second >actual &&
+		echo "second" >expect &&
+		test_cmp expect actual
+	)
+'
+
 test_done
