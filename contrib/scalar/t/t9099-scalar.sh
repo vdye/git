004649f789dd01a9d6280f36087e137bfaf886fd
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-05-06 14:35:12 +0200

    scalar: disable authentication in unattended mode
    Modified to remove call to is_unattended() that has not been implemented
    yet.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/scalar/t/t9099-scalar.sh
+++ contrib/scalar/t/t9099-scalar.sh
@@ -13,6 +13,13 @@ PATH=$(pwd)/..:$PATH
 GIT_TEST_MAINT_SCHEDULER="crontab:test-tool crontab ../cron.txt"
 export GIT_TEST_MAINT_SCHEDULER
 
+# Do not write any files outside the trash directory
+Scalar_UNATTENDED=1
+export Scalar_UNATTENDED
+
+GIT_ASKPASS=true
+export GIT_ASKPASS
+
 test_lazy_prereq BUILTIN_FSMONITOR '
 	git version --build-options | grep -q "feature:.*fsmonitor--daemon"
 '
