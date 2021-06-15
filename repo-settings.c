aa9ba7021d67376ce2432226c8cec0da6d5177d6
Derrick Stolee <dstolee@microsoft.com>
2021-06-15 11:07:11 -0400

    repo-settings: enable sparse index by default
    There is some strangeness when expanding a sparse-index that exists
    within a submodule. We will need to resolve that later, but for now,
    let's do a better job of explicitly disabling the sparse-index when
    requested, and do so in t7817.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- repo-settings.c
+++ repo-settings.c
@@ -70,7 +70,7 @@ void prepare_repo_settings(struct repository *r)
 	repo_cfg_bool(r, "fetch.writecommitgraph", &r->settings.fetch_write_commit_graph, 0);
 	repo_cfg_bool(r, "pack.usesparse", &r->settings.pack_use_sparse, 1);
 	repo_cfg_bool(r, "core.multipackindex", &r->settings.core_multi_pack_index, 1);
-	repo_cfg_bool(r, "index.sparse", &r->settings.sparse_index, 0);
+	repo_cfg_bool(r, "index.sparse", &r->settings.sparse_index, 1);
 
 	/*
 	 * The GIT_TEST_MULTI_PACK_INDEX variable is special in that
