72061b80a658657235afa628d9889ee6915e3dba
Victoria Dye <vdye@github.com>
2021-08-03 22:31:06 -0400

    fixup! scalar register: set recommended config settings
    Fix  to allow non- workdirs

--- contrib/scalar/scalar.c
+++ contrib/scalar/scalar.c
@@ -19,55 +19,91 @@ static int is_unattended(void) {
 	return git_env_bool("Scalar_UNATTENDED", 0);
 }
 
+/*
+ * Remove the deepest subdirectory in the provided path string. Path must not
+ * include a trailing path separator. Returns 1 if parent directory found,
+ * otherwise 0.
+ */
+static int strbuf_parentdir(struct strbuf *buf)
+{
+	size_t len = buf->len;
+	size_t offset = offset_1st_component(buf->buf);
+	char *path_sep = find_last_dir_sep(buf->buf + offset);
+	strbuf_setlen(buf, path_sep ? path_sep - buf->buf : offset);
+
+	return buf->len < len;
+}
+
 static void setup_enlistment_directory(int argc, const char **argv,
 				       const char * const *usagestr,
-				       const struct option *options)
+				       const struct option *options,
+				       struct strbuf *enlistment_root)
 {
+	struct strbuf path = STRBUF_INIT;
+	char *root;
+	int enlistment_found = 0;
+
 	if (startup_info->have_repository)
 		BUG("gitdir already set up?!?");
 
 	if (argc > 1)
 		usage_with_options(usagestr, options);
 
+	/* find the worktree, determine its corresponding root */
 	if (argc == 1) {
-		char *src = xstrfmt("%s/src", argv[0]);
-		const char *dir = is_directory(src) ? src : argv[0];
-
-		if (chdir(dir) < 0)
-			die_errno(_("could not switch to '%s'"), dir);
-
-		free(src);
-	} else {
-		/* find the worktree, and ensure that it is named `src` */
-		struct strbuf path = STRBUF_INIT;
-
-		if (strbuf_getcwd(&path) < 0)
-			die(_("need a working directory"));
-
-		for (;;) {
-			size_t len = path.len;
-
-			strbuf_addstr(&path, "/src/.git");
-			if (is_git_directory(path.buf)) {
-				strbuf_setlen(&path, len);
-				strbuf_addstr(&path, "/src");
-				if (chdir(path.buf) < 0)
-					die_errno(_("could not switch to '%s'"),
-						  path.buf);
-				strbuf_release(&path);
-				break;
-			}
-
-			while (len > 0 && !is_dir_sep(path.buf[--len]))
-				; /* keep looking for parent directory */
-
-			if (!len)
-				die(_("could not find enlistment root"));
-
-			strbuf_setlen(&path, len);
-		}
+		strbuf_add_absolute_path(&path, argv[0]);
+	} else if (strbuf_getcwd(&path) < 0) {
+		die(_("need a working directory"));
 	}
 
+	strbuf_trim_trailing_dir_sep(&path);
+	do {
+		const size_t len = path.len;
+
+		/* check if currently in enlistment root with src/ workdir */
+		strbuf_addstr(&path, "/src/.git");
+		if (is_git_directory(path.buf)) {
+			strbuf_strip_suffix(&path, "/.git");
+
+			if (enlistment_root)
+				strbuf_add(enlistment_root, path.buf, len);
+
+			enlistment_found = 1;
+			break;
+		}
+
+		/* reset to original path */
+		strbuf_setlen(&path, len);
+
+		/* check if currently in workdir */
+		strbuf_addstr(&path, "/.git");
+		if (is_git_directory(path.buf)) {
+			strbuf_setlen(&path, len);
+
+			if (enlistment_root) {
+				/*
+				 * If the worktree's directory's name is `src`, the enlistment is the
+				 * parent directory, otherwise it is identical to the worktree.
+				 */
+				root = strip_path_suffix(path.buf, "src");
+				strbuf_addstr(enlistment_root, root ? root : path.buf);
+				free(root);
+			}
+
+			enlistment_found = 1;
+			break;
+		}
+
+		strbuf_setlen(&path, len);
+	} while (strbuf_parentdir(&path));
+
+	if (!enlistment_found)
+		die(_("could not find enlistment root"));
+
+	if (chdir(path.buf) < 0)
+		die_errno(_("could not switch to '%s'"), path.buf);
+
+	strbuf_release(&path);
 	setup_git_directory();
 }
 
@@ -1299,7 +1335,7 @@ static int cmd_register(int argc, const char **argv)
 	argc = parse_options(argc, argv, NULL, options,
 			     usage, 0);
 
-	setup_enlistment_directory(argc, argv, usage, options);
+	setup_enlistment_directory(argc, argv, usage, options, NULL);
 
 	return register_dir();
 }
