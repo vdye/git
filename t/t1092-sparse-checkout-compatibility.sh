e15621ecb1345be5c3e59956ffe6b30aec93816a
Victoria Dye <vdye@github.com>
2021-09-15 14:18:23 -0400

    read-tree: narrow scope of index expansion for `--prefix`
    When `unpack_trees` is provided with a prefix, the tree traversal that occurs
    in it requires that the base directory path given to it is a traversable
    directory in the index. In order to adhere to this requirement, the index is
    expanded if the given prefix is equal to or a subdirectory of a sparse
    directory in the index.
    
    Signed-off-by: Victoria Dye <vdye@github.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -1340,7 +1340,13 @@ test_expect_success 'sparse index is not expanded: read-tree' '
 	do
 		ensure_not_expanded read-tree -mu $MERGE_TREES &&
 		ensure_not_expanded reset --hard HEAD || return 1
-	done
+	done &&
+
+	rm -rf sparse-index/deep/deeper2 &&
+	ensure_not_expanded add . &&
+	ensure_not_expanded commit -m "test" &&
+
+	ensure_not_expanded read-tree --prefix=deep/deeper2 -u deepest
 '
 
 # NEEDSWORK: a sparse-checkout behaves differently from a full checkout
