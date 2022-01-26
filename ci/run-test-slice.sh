a6106525b7f7f33480a410a630806a321f355fc8
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-01-26 19:23:56 +0100

    ci/run-test-slice.sh: replace shelling out with "echo"
    Amend this code added in b819f1d2cec (ci: parallelize testing on
    Windows, 2019-01-29) to stop shelling out to "tr", all we're trying to
    do here is to turn '\n'-delimited output into a ' '-delimited list. To
    do that we can just use "echo".
    
    None of these files will contain the odd characters using "echo" might
    choke on in the general case (i.e. '\'-escapes and the like).
    
    This has the added benefit of not adding a redundant ' ' to the end of
    the T-parameter, which didn't matter in practice, but as we'll see in
    a subsequent commit makes for slightly nicer output, as we'll show
    this variable using a GitHub CI-native feature.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- ci/run-test-slice.sh
+++ ci/run-test-slice.sh
@@ -5,6 +5,6 @@
 
 . ${0%/*}/lib.sh
 
-make --quiet -C t T="$(cd t &&
-	./helper/test-tool path-utils slice-tests "$1" "$2" t[0-9]*.sh |
-	tr '\n' ' ')"
+tests=$(echo $(cd t && ./helper/test-tool path-utils slice-tests "$1" "$2" \
+	t[0-9]*.sh))
+make --quiet -C t T="$tests"
